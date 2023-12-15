0eda3b06e8a97e7de1cf83f46f2900f386f4a473
Bartlomiej Kaszowski <bartlomiej.kaszowski@gmail.com>
2023-12-15 20:39:06 +0100

    Initial commit

---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from './dto/payments-create-change.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2023-10-16',
+    },
+  );
+
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
+
+  async createCharge({ amount, email }: PaymentsCreateChargeDto) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: 'usd',
+      confirm: true,
+      payment_method: 'pm_card_visa',
+      return_url: 'https://google.com',
+    });
+    this.notificationsService.emit('notify_email', {
+      email,
+      text: `Your payment of $${amount} has completed successfully`,
+    });
+    return paymentIntent;
+  }
+}
---
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsDate,
+  IsDefined,
+  IsNotEmptyObject,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { CreateChargeDto } from '@app/common';
+
+//@Type transforms the type of data
+export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date;
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date;
+
+  @IsDefined()
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => CreateChargeDto)
+  charge: CreateChargeDto;
+}
